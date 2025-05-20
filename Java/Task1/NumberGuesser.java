edb27340afa4afd7a0bdafd1c7505a44f2ca8273
Akshitha-1023 <akshithaindian@gmail.com>
2025-05-20 13:03:08 +0530

    Added loop for multiple rounds and random number

--- Java/Task1/NumberGuesser.java
+++ Java/Task1/NumberGuesser.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class NumberGame {
   public static void main(String[] args) {
@@ -9,5 +10,16 @@ public class NumberGame {
 
     System.out.println("Welcome to the Number Guessing Game!");
   }
+}
+
+while (playAgain) {
+  int targetNumber = (int) (Math.random()*100) + 1;
+  int guess = 0;
+  int attempts = 0;
+  int maxAttempts = 10;
+  boolean hasGuessed = false;
+
+  System.out.println("\nI've picked a number between 1 and 100. Can you guess it?");
+  System.out.println("You have " + maxAttempts + "attempts.");
 }
     
