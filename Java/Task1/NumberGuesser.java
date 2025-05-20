b6f09505acfb045dd569b8fec5753c6769a633c9
Akshitha-1023 <akshithaindian@gmail.com>
2025-05-20 12:07:33 +0530

    Update NumberGuesser.java
    Initial setup: class and main method

--- Java/Task1/NumberGuesser.java
+++ Java/Task1/NumberGuesser.java
@@ -1,7 +1,8 @@
-import java.util.Random;
-
+import java.util.Scanner;
+//Step 1: Basic structure
 public class NumberGuesser {
   public static void main(String[] args) {
+    //Entry Point
     System.out.println("Game starts soon!");
   }
 }
